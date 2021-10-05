6f7bb633fd5868b4b43ecb6f279cc04302656a09
Наиль <nailuspnov@gmail.com>
2021-10-05 12:14:25 +0400

    Add model class and init FieldTable

---
+++ src/tictactoe/model/Cell.java
@@ -0,0 +1,19 @@
+package tictactoe.model;
+
+public class Cell {
+    private final int row;
+    private final int col;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+}
--- src/tictactoe/model/FieldTable.java
+++ src/tictactoe/model/FieldTable.java
@@ -1,4 +1,13 @@
 package tictactoe.model;
 
 public class FieldTable {
+    private final char[][] table = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+    };
+
+    public boolean isEmpty(Cell cell) {
+        return table[cell.getRow()][cell.getCol()] == ' ';
+    }
 }
