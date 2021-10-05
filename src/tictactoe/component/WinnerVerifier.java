33e76b18ba83b09489eb7a440fca11ea5be85245
Наиль <nailuspnov@gmail.com>
2021-10-05 14:51:57 +0400

    Implement ComputerWin verify

--- src/tictactoe/component/WinnerVerifier.java
+++ src/tictactoe/component/WinnerVerifier.java
@@ -7,7 +7,31 @@ public class WinnerVerifier {
     public boolean isUserWin;
 
     public boolean isComputerWin(FieldTable table) {
-        return true;
+        return (winnerByCols(table) || winnerByDiag(table) || winnerByRows(table));
+    }
+
+    private boolean winnerByRows(FieldTable table) {
+        for (int i = 0; i < 3; i++) {
+            if (table.getTable()[i][0] == 'O' && table.getTable()[i][1] == 'O' && table.getTable()[i][2] == 'O') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean winnerByCols(FieldTable table) {
+        for (int i = 0; i < 3; i++) {
+            if (table.getTable()[0][i] == 'O' && table.getTable()[1][i] == 'O' && table.getTable()[2][i] == 'O') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean winnerByDiag(FieldTable table) {
+        if (table.getTable()[0][0] == 'O' && table.getTable()[1][1] == 'O' && table.getTable()[2][2] == 'O')
+            return true;
+        return table.getTable()[0][2] == 'O' && table.getTable()[1][1] == 'O' && table.getTable()[2][0] == 'O';
     }
 
     public boolean isUserWin(FieldTable table) {
